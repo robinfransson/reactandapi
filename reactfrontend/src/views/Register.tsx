9462b1b6747b8db3c34ec33804e7bcd5c81e9f20
Robin Fransson <robinf96@gmail.com>
2022-02-26 20:39:05 +0100

    fix meesage modal

--- reactfrontend/src/views/Register.tsx
+++ reactfrontend/src/views/Register.tsx
@@ -1,66 +1,73 @@
-import * as React from "react";
-import { useForm } from "react-hook-form";
-import { API, CreateUserCommand, CreateUserResult } from "../scripts/api";
-import "../scss/Register.scss";
+import * as React from 'react';
+import { useForm } from 'react-hook-form';
+import { API, CreateUserCommand, CreateUserResult } from '../scripts/api';
+import '../scss/Register.scss';
 export const Register: React.FC<{}> = () => {
-  const { register, handleSubmit } = useForm<CreateUserCommand>();
-  const [result, setResult] = React.useState<CreateUserResult>();
+    const { register, handleSubmit } = useForm<CreateUserCommand>();
+    const [result, setResult] = React.useState<CreateUserResult>();
+    const timeout = 5000;
+    React.useEffect(() => {}, [result]);
 
-  React.useEffect(() => {}, [result]);
-
-  const onSubmit = async (data: CreateUserCommand) => {
-    API.createUser(data).then((res) => {
-      setResult(res);
-    });
-  };
-  return (
-    <div className="Register-main">
-      {result && (
-        <Message message={result.message} status={result.status} delay={5000} />
-      )}
-      <form onSubmit={handleSubmit(onSubmit)} className="Register-form">
-        <label>
-          Email
-          <input {...register("email")}></input>
-        </label>
-        <label>
-          Username
-          <input {...register("username")}></input>
-        </label>
-        <label>
-          Password
-          <input {...register("password")}></input>
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-  );
+    const onSubmit = async (data: CreateUserCommand) => {
+        API.createUser(data).then((res) => {
+            setResult(res);
+            setTimeout(() => {
+                setResult(undefined);
+            }, timeout);
+        });
+    };
+    return (
+        <div className="Register-main">
+            {result && (
+                <Message
+                    message={result.message}
+                    status={result.status}
+                    delay={timeout}
+                />
+            )}
+            <form onSubmit={handleSubmit(onSubmit)} className="Register-form">
+                <label>
+                    Email
+                    <input {...register('email')}></input>
+                </label>
+                <label>
+                    Username
+                    <input {...register('username')}></input>
+                </label>
+                <label>
+                    Password
+                    <input {...register('password')}></input>
+                </label>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
 };
 interface mess {
-  message: string;
-  status: number;
-  delay: number;
+    message: string;
+    status: number;
+    delay: number;
 }
 function Message(info: mess) {
-  const [visible, setVisible] = React.useState<boolean>(true);
-  console.table(info);
+    const [visible, setVisible] = React.useState<boolean>(true);
+    console.table(info);
 
-  React.useEffect(() => {
-    setTimeout(() => {
-      setVisible(false);
-    }, info.delay);
-  }, [visible]);
+    React.useEffect(() => {
+        setTimeout(() => {
+            setVisible(false);
+        }, info.delay);
+    }, [visible]);
 
-  return visible ? (
-    <div
-      className={[
-        "Message",
-        info.status == 0 ? "Message-success" : "Message-error",
-      ].join(" ")}
-    >
-      {info.message}
-    </div>
-  ) : (
-    <></>
-  );
+    return visible ? (
+        <div
+            className={[
+                'Message',
+                info.status == 0 ? 'Message--success' : 'Message--error',
+            ].join(' ')}
+        >
+            {info.message}
+        </div>
+    ) : (
+        <></>
+    );
 }
