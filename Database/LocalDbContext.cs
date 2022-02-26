72396b1a5e4b72eafd51878d25b2bb9be7b85527
Robin Fransson <robinf96@gmail.com>
2022-02-26 16:58:38 +0100

    add db

---
+++ Database/LocalDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database;
+
+public class LocalDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<UserAuthentication> UserAuthentications { get; set; }
+
+    private string DbPath { get; }
+
+    public LocalDbContext()
+    {
+        var path = Directory.GetCurrentDirectory();
+        DbPath = Path.Join(path, "test.db");
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
---
+++ reactfrontend/src/views/Register.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import { API } from "../scripts/api";
+export const Register: React.FC<{}> = () => {
+  React.useEffect(() => {});
+  return <div className="Register-main">
+      <form className="Register-form">
+          <div className
+          </form>  </div>;
+};
